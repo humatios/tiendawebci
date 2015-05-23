ab02eb07f1c345b187841ffdc20bb29842d97fdb
Nuevo <Nuevo@Nuevo-PC.Home>
2015-05-23 12:48:29 -0500

    Prueba

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-//Control
+//Control Prueba Branch
 class Admin extends CI_Controller {
 
     public function __construct() {
